8a19938f28a165bdb9e1c34ecad66a2b5ead6352
mason2mr <112265436+mason2mr@users.noreply.github.com>
2022-09-18 19:59:15 -0400

    Add files via upload

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,17 @@
+﻿$Hello = "Hello, PowerShell"
+Write-Host($Hello)
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+## confirm the popup with "Yes"
+ Get-NetIPAddress
+ get-help get-netipaddress
+ get-netipaddress.ipaddress
+function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+write-host(getIP)
+$IP = getIP
+
+Write-Host("This machine's IP is $IP")
+
+Write-Host("This machine's IP is {0}" -f $IP)
