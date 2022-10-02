8f8a18ec02d7d0839f026c3281fc4615c389593a
mason2mr <112265436+mason2mr@users.noreply.github.com>
2022-10-02 14:48:43 -0400

    Add files via upload

---
+++ python/nameAge.py
@@ -0,0 +1,8 @@
+print('What is your name?')
+myName = input()
+print('Hello, ' + myName + '. That is a good name. How old are you?')
+myAge = input()
+
+print('%s? That\'s funny, I\'m only a few seconds old.' % myAge)
+
+print("I wish I was %s years old" % (myAge * 2))M
