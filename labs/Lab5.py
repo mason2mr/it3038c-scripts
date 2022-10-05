2624046d97f2a2fd41ffd5c66e5944bf865fb0be
mason2mr <mason2mr@mail.uc.edu>
2022-10-05 16:26:40 -0400

    Changes Made

--- labs/Lab5.py
+++ labs/Lab5.py
@@ -1,13 +1,15 @@
 #Take a number input and calulate how many prime numbers come between it and 0.
 
-lower_value = int(input ("Please, Enter the Lowest Range Value: "))  
+# lower_value = int(input ("Please, Enter the Lowest Range Value: "))  
 upper_value = int(input ("Please, Enter the Upper Range Value: "))  
   
-print ("The Prime Numbers in the range are: ")  
-for number in range (lower_value, upper_value + 1):  
+print ("The Prime Numbers in the range are: ") 
+Count = 0
+for number in range (0, upper_value + 1):  
     if number > 1:  
         for i in range (2, number):  
             if (number % i) == 0:  
                 break  
-        else:  
-            print (number) 
+        else:   
+            Count = Count+1  
+print (Count)
--- testing/app.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-Console.log('Hello');
-=======
-Console.log('Hello');
->>>>>>> 8f8a18ec02d7d0839f026c3281fc4615c389593a
