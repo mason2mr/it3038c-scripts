33504a3a627357befb7948a1ed04f347c7d37c22
mason2mr <112265436+mason2mr@users.noreply.github.com>
2022-10-30 21:06:22 -0400

    Update app.py

--- python/Project2/app.py
+++ python/Project2/app.py
@@ -1,8 +1,8 @@
-# Source For Code used: https://www.fullstackpython.com/flask-templating-render-template-examples.htmlfrom flask import Flask,render_template,request
+#Cited Source For Code: https://www.fullstackpython.com/flask-templating-render-template-examples.htmlfrom flask import Flask,render_template,request
 import time
 today = time.strftime("%Y-%m-%d") #Coverts the time of today to the actual year, month, and day   
 current_time = time.strftime("%d %B %Y %H:%M:%S %Z %I:%M %p") #Converts current time to their representation
-print("Myron Today's Date And Time is Currently:") # Displays my personalized scripy
+print("Myron Today's Date And Time is Currently:") # Displays my personalized script
 print(current_time) #Displays current time when ran
 app = Flask(__name__)
 
@@ -24,5 +24,5 @@ import time   #imports Time
 
 today = time.strftime("%Y-%m-%d") #Coverts the time of today to the actual year, month, and day   
 current_time = time.strftime("%d %B %Y %H:%M:%S %Z %I:%M %p") #Converts current time to their representation
-print("Myron Today's Date And Time is Currently:") # Displays my personalized scripy
+print("Myron Today's Date And Time is Currently:") # Displays my personalized script
 print(current_time) #Displays current time when ran
